c7e74b80aed60c834d28094fcfbaab9e55728460
Dorian Birch <206dorian@gmail.com>
2024-01-12 15:19:52 -0600

    adding form and info to about us page

--- zen-mindset/app.py
+++ zen-mindset/app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template
 import tkinter as tk
 
 app = Flask(__name__)
-
 # Configure templates directory (optional)
 app.template_folder = "templates"
 
@@ -10,5 +9,12 @@ app.template_folder = "templates"
 def index():
     return render_template("index.html")
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/resources")
+def resources():
+    return render_template("resources.html")
 if __name__ == "__main__":
     app.run()
